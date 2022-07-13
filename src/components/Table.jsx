6b2d4923076ca168d0b997981fb5d951cd79b9f2
Tarick <gabre.tarick@gmail.com>
2022-07-13 16:29:52 -0300

    requisito 3. add filtros a tabela

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,10 +1,14 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import TableContext from '../context/TableContext';
 
+const columnFilter = ['population', 'orbital_period',
+  'diameter', 'rotation_period', 'surface_water'];
+
 export default function Table() {
-  const { data } = useContext(TableContext);
-  const [filterName, setFilterName] = useState('');
-  const handleFilterName = ({ target: { value } }) => setFilterName(value);
+  const { filterByName: { name }, handleFilterName,
+    filterData, filterColumn, handleFilterColumn,
+    filterComparison, handleFilterComparison, filterQuantity,
+    handleFilterQuantity, filterSubmit } = useContext(TableContext);
 
   return (
     <div>
@@ -15,9 +19,45 @@ export default function Table() {
           type="text"
           name="filterByName"
           data-testid="name-filter"
+          value={ name }
           onChange={ handleFilterName }
         />
       </label>
+      <select
+        data-testid="column-filter"
+        value={ filterColumn }
+        onChange={ handleFilterColumn }
+      >
+        {columnFilter.map((filter, index) => (
+          <option key={ index }>{filter}</option>
+        ))}
+      </select>
+      <select
+        data-testid="comparison-filter"
+        value={ filterComparison }
+        onChange={ handleFilterComparison }
+      >
+        <option value="maior que">maior que</option>
+        <option value="menor que">menor que</option>
+        <option value="igual a">igual a</option>
+      </select>
+      <label htmlFor="quantity">
+        Quantity:
+        <input
+          id="quantity"
+          type="number"
+          data-testid="value-filter"
+          value={ filterQuantity }
+          onChange={ handleFilterQuantity }
+        />
+      </label>
+      <button
+        type="button"
+        data-testid="button-filter"
+        onClick={ filterSubmit }
+      >
+        Filter:
+      </button>
       <table>
         <thead>
           <tr>
@@ -37,25 +77,23 @@ export default function Table() {
           </tr>
         </thead>
         <tbody>
-          {data.filter((planet) => planet.name.toLowerCase()
-            .includes(filterName.toLowerCase()))
-            .map((planet, index) => (
-              <tr key={ index }>
-                <td>{planet.name}</td>
-                <td>{planet.rotation_period}</td>
-                <td>{planet.orbital_period}</td>
-                <td>{planet.diameter}</td>
-                <td>{planet.climate}</td>
-                <td>{planet.gravity}</td>
-                <td>{planet.terrain}</td>
-                <td>{planet.surface_water}</td>
-                <td>{planet.population}</td>
-                <td>{planet.films}</td>
-                <td>{planet.created}</td>
-                <td>{planet.edited}</td>
-                <td>{planet.url}</td>
-              </tr>
-            ))}
+          {filterData.map((planet, index) => (
+            <tr key={ index }>
+              <td>{planet.name}</td>
+              <td>{planet.rotation_period}</td>
+              <td>{planet.orbital_period}</td>
+              <td>{planet.diameter}</td>
+              <td>{planet.climate}</td>
+              <td>{planet.gravity}</td>
+              <td>{planet.terrain}</td>
+              <td>{planet.surface_water}</td>
+              <td>{planet.population}</td>
+              <td>{planet.films}</td>
+              <td>{planet.created}</td>
+              <td>{planet.edited}</td>
+              <td>{planet.url}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
--- src/context/TableProvider.js
+++ src/context/TableProvider.js
@@ -4,6 +4,8 @@ import TableContext from './TableContext';
 
 export default function TableProvider({ children }) {
   const [data, setData] = useState([]);
+  const [filterByName, setFilterByName] = useState('');
+  const handleFilterName = ({ target }) => setFilterByName(target.value);
 
   useEffect(() => {
     const getData = async () => {
@@ -16,13 +18,63 @@ export default function TableProvider({ children }) {
     getData();
   }, []);
 
-  const typeContext = {
+  const [filterData, setFilterData] = useState([...data]);
+  const [filterByNumberValues, setFilterByNumberValues] = useState([]);
+
+  useEffect(() => {
+    const nameByFilter = data.filter((planet) => (
+      planet.name.toLowerCase().includes(filterByName)
+    ));
+
+    const newList = filterByNumberValues.reduce((acc, index) => acc.filter((planet) => {
+      switch (index.comparison) {
+      case 'maior que':
+        return Number(planet[index.column]) > Number(index.value);
+      case 'menor que':
+        return Number(planet[index.column]) < Number(index.value);
+      case 'igual a':
+        return Number(planet[index.column]) === Number(index.value);
+      default:
+        return planet;
+      }
+    }), nameByFilter);
+    setFilterData(newList);
+  }, [data, filterByName, filterByNumberValues]);
+
+  const [filterColumn, setFilterColumn] = useState('population');
+  const handleFilterColumn = ({ target }) => { setFilterColumn(target.value); };
+
+  const [filterComparison, setFilterComparison] = useState('maior que');
+  const handleFilterComparison = ({ target }) => { setFilterComparison(target.value); };
+
+  const [filterQuantity, setFilterQuantity] = useState(0);
+  const handleFilterQuantity = ({ target }) => { setFilterQuantity(target.value); };
+
+  const filterSubmit = () => {
+    const newFilter = {
+      column: filterColumn,
+      comparison: filterComparison,
+      value: filterQuantity,
+    };
+    setFilterByNumberValues([...filterByNumberValues, newFilter]);
+  };
+
+  const contextValue = {
     data,
-    setData,
+    filterByName,
+    handleFilterName,
+    filterData,
+    filterColumn,
+    handleFilterColumn,
+    filterComparison,
+    handleFilterComparison,
+    filterQuantity,
+    handleFilterQuantity,
+    filterSubmit,
   };
 
   return (
-    <TableContext.Provider value={ typeContext }>
+    <TableContext.Provider value={ contextValue }>
       {children}
     </TableContext.Provider>
   );
